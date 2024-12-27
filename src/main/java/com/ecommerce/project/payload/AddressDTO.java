3442583ff8f9a0c5135aa0b676fe3a4c311a7ddd
shamsheruhani1 <shamsheruhani1@gmail.com>
2024-12-27 19:25:13 +0530

    Added Address module : increased JWT Token Expired Time

---
+++ src/main/java/com/ecommerce/project/payload/AddressDTO.java
@@ -0,0 +1,38 @@
+package com.ecommerce.project.payload;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressDTO {
+    private Long addressId;
+
+    @NotBlank
+    @Size(min = 5, message = "Street name must be atleast 5 characters")
+    private String street;
+
+    @NotBlank
+    @Size(min = 5, message = "Building name must be atleast 5 characters")
+    private String buildingName;
+
+    @NotBlank
+    @Size(min = 4, message = "City name must be atleast 4 characters")
+    private String city;
+
+    @NotBlank
+    @Size(min = 2, message = "State name must be atleast 2 characters")
+    private String state;
+
+    @NotBlank
+    @Size(min = 2, message = "Country name must be atleast 2 characters")
+    private String country;
+
+    @NotBlank
+    @Size(min = 6, max = 6, message = "Pincode must be atleast 6 characters")
+    private String pincode;
+}
